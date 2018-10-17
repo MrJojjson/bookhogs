61e8a4d57b51fc4303607d059c11435dbc4d8c9e
MrJojjson <johansjoberg88@gmail.com>
2018-10-17 22:22:23 +0200

    Remove api

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,10 +1,7 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8ZfC_bBnU8LslTWpQtPFN6mCScpmiI5Y",
-  authDomain: "bookhogs-2be58.firebaseapp.com",
-  databaseURL: "https://bookhogs-2be58.firebaseio.com",
-  storageBucket: "bookhogs-2be58.appspot.com",
+  
 };
 
 var firebaseApp = null;
